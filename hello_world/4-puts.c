697ac62367b99d8bc8146e3de8fb7e72ac4c0d46
lopezmariegabrielle-oss <lopezmariegabrielle@gmail.com>
2026-02-23 11:10:35 +0000

    Add task 4

---
+++ hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Point d'entrée du programme
+ *
+ * Return: Toujours 0 (Succès)
+ */
+int main(void)
+{
+puts("Programming is like building a multilingual puzzle");
+return (0);
+}
