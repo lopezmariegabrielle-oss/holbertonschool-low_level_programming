c9746105c9c74587fb5a6d6c8df1fa6e94b8c59c
lopezmariegabrielle-oss <lopezmariegabrielle@gmail.com>
2026-02-25 15:02:08 +0000

    Add task 3

--- functions_nested_loops/main.c
+++ functions_nested_loops/main.c
@@ -6,7 +6,20 @@
 */
 int main(void)
 {
+int r;
+/*execice 1*/
 print_alphabet();
+/*exercice 2*/
 print_alphabet_x10();
+/*exercice 3*/
+    r = _islower('a'); /* Devrait renvoyer 1 */
+    _putchar(r + '0');
+    r = _islower('A'); /* Devrait renvoyer 0 */
+    _putchar(r + '0');
+    r = _islower('z'); /* Devrait renvoyer 1 */
+    _putchar(r + '0');
+    _putchar('\n');
+    return (0);
+
 return (0);
 }
