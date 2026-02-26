68a32455388d32163c6d43f1ae344c2a64a5d80b
lopezmariegabrielle-oss <lopezmariegabrielle@gmail.com>
2026-02-26 12:01:24 +0000

    Add task 10

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * add - additionne deux entiers
+ * @a: premier entier
+ * @b: deuxième entier
+ *
+ * Return: le résultat de l'addition
+ */
+int add(int a, int b)
+{
+return (a + b);
+}
