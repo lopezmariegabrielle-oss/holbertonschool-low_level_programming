a7249320306695bb21be9327814dbae7ff61b153
lopezmariegabrielle-oss <lopezmariegabrielle@gmail.com>
2026-02-26 08:40:01 +0000

    Add task 7

--- functions_nested_loops/main.c
+++ functions_nested_loops/main.c
@@ -57,6 +57,13 @@ r = _isalpha('H');
 
     r = _abs(98);
     _putchar('\n');
+
+/*exercice 7*/
+print_last_digit(98);
+    print_last_digit(0);
+    r = print_last_digit(-1024);
+    _putchar('0' + r);
+    _putchar('\n');
     
 return (0);
 }
