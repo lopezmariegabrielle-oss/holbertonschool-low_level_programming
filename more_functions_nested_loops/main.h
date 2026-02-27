d5a97b2bd5a6bd903b0a43fe11e436bdcdae68a0
lopezmariegabrielle-oss <lopezmariegabrielle@gmail.com>
2026-02-27 08:54:51 +0000

    Add task 0

---
+++ more_functions_nested_loops/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+/**
+ * main.h - Header file containing prototypes for all functions
+ * used in the project.
+ */
+int _putchar(char c);
+int _isupper(int c);
+
+#endif
