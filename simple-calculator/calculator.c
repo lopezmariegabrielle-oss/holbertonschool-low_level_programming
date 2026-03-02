d2e16c3a4d37e52cec64523ec3efe2d4efd84fbb
lopezmariegabrielle-oss <lopezmariegabrielle@gmail.com>
2026-03-02 14:11:20 +0000

    Add task 0

--- simple-calculator/calculator.c
+++ simple-calculator/calculator.c
@@ -7,11 +7,11 @@
  */
 int main(void)
 {
-	int choice;
+	int choice = 0;
 
 	printf("Simple Calculator\n");
 	do {
-		printf("1) Add\n2) Substract\n3) Multiply\n4) Divide\n0) Quit\n");
+		printf("1) Add\n2) Subtract\n3) Multiply\n4) Divide\n0) Quit\n");
 		printf("Choice: ");
 		scanf("%d", &choice);
 		if (choice == 0)
