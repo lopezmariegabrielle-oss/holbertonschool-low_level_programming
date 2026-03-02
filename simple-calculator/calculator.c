bac12c71d2a2204559a0ee1256f736494a32552b
lopezmariegabrielle-oss <lopezmariegabrielle@gmail.com>
2026-03-02 12:41:17 +0000

    Add task 0

--- simple-calculator/calculator.c
+++ simple-calculator/calculator.c
@@ -1,64 +1,61 @@
 #include <stdio.h>
 
-int main(void) 
+/**
+ * effectuer_calcul - Gère les opérations de calcul
+ * @choix: L'opération choisie
+ * @a: Premier nombre
+ * @b: Deuxième nombre
+ */
+void effectuer_calcul(int choix, int a, int b)
 {
-int choix;
-int a, b;
-
-printf("Simple Calculator\n");
-do
-{
-printf("1) Add\n2) Subtract\n3) Multiply\n4) Divide\n0) Quit\nChoice: ");
-scanf("%d", &choix);
-
-switch (choix)
-{
-case 0:
-printf("Bye!\n");
-break;
-case 1:
-printf("Enter first number: ");
-scanf("%d", &a);
-printf("Enter second number: ");
-scanf("%d", &b);
-printf("Result: %d\n", a + b);
-break;
-case 2:
-printf("Enter first number: ");
-scanf("%d", &a);
-printf("Enter second number: ");
-scanf("%d", &b);
-printf("Result: %d\n", a - b);
-break;
-case 3:
-printf("Enter first number: ");
-scanf("%d", &a);
-printf("Enter second number: ");
-scanf("%d", &b);
-printf("Result: %d\n", a * b);
-break;
-case 4:
-printf("Enter first number: ");
-scanf("%d", &a);
-printf("Enter second number: ");
-scanf("%d", &b);
-
-
-if (b == 0)
-{
-printf("Error: Division by zero\n");
+	switch (choix)
+	{
+	case 1:
+		printf("Result: %d\n", a + b);
+		break;
+	case 2:
+		printf("Result: %d\n", a - b);
+		break;
+	case 3:
+		printf("Result: %d\n", a * b);
+		break;
+	case 4:
+		if (b == 0)
+			printf("Error: Division by zero\n");
+		else
+			printf("Result: %d\n", a / b);
+		break;
+	}
 }
-else
-{
-printf("Result: %d\n", a / b);
-}
-break;
-default:
-printf("Invalid choice, please try again.\n");
-break;
-}
-}
-while (choix !=0);
 
-return 0;
+/**
+ * main - Point d'entrée du programme
+ *
+ * Return: Toujours 0 (Succès)
+ */
+int main(void)
+{
+	int choix, a, b;
+
+	printf("Simple Calculator\n");
+	do {
+		printf("1) Add\n2) Subtract\n3) Multiply\n4) Divide\n0) Quit\nChoice: ");
+		scanf("%d", &choix);
+
+		if (choix >= 1 && choix <= 4)
+		{
+			printf("Enter first number: ");
+			scanf("%d", &a);
+			printf("Enter second number: ");
+			scanf("%d", &b);
+			effectuer_calcul(choix, a, b);
+		}
+		else if (choix == 0)
+			printf("Bye!\n");
+		else
+			printf("Invalid choice, please try again.\n");
+
+	} while (choix != 0);
+
+	return (0);
 }
