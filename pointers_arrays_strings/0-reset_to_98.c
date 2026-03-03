14d29a7a27fc1cd4e3bd9cf0420e1756c1f9626a
lopezmariegabrielle-oss <lopezmariegabrielle@gmail.com>
2026-03-03 12:40:07 +0000

    Add task 1

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * reset_to_98 - met à jour la valeur pointée par n à 98
+ * @n: pointeur vers l'entier à modifier
+ *
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+*n = 98;
+}
---
+++ pointers_arrays_strings/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+/**
+ * file: main.h
+ * description: Header file containing prototypes for all functions
+ * used in the pointers and arrays project.
+ */
+
+int _putchar(char c);
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+
+#endif
