3444c6210c8cc9fa9ae19e0c0e8a3dbba7902a29
lopezmariegabrielle-oss <lopezmariegabrielle@gmail.com>
2026-03-04 11:28:51 +0000

    Add task 5

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -6,17 +6,22 @@
  */
 void rev_string(char *s)
 {
-	int len = 0;
-	int i = 0;
-	char temp;
-    while (s[len] != '\0')
+int len = 0;
+int i = 0;
+char temp;
+
+	if (s == NULL)
 	{
-		len++;
+		return;
 	}
-    for (i = 0; i < len / 2; i++)
-    {
-        temp = s[i];
-		s[i] = s[len - 1 - i];
-		s[len - 1 - i] = temp;
-    }
+while (s[len] != '\0')
+{
+len++;
+}
+for (i = 0; i < len / 2; i++)
+{
+temp = s[i];
+s[i] = s[len - 1 - i];
+s[len - 1 - i] = temp;
+}
 }
