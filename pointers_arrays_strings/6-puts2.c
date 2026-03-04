31e0513b51e0228f7a52333a7a5ae3cdb9db57fa
lopezmariegabrielle-oss <lopezmariegabrielle@gmail.com>
2026-03-04 11:38:41 +0000

    Add task 6

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -1,26 +1,29 @@
 #include "main.h"
 
 /**
- * puts2 - affiche un caractère sur deux d'une chaîne
- * @str: la phrase qu'on me donne à traiter
+ * puts2 - prints every other character of a string
+ * @str: the string to be treated
  */
 void puts2(char *str)
 {
 	int i = 0;
-/*Je parcours la phrase en affichant une lettre, 
-* puis je saute la suivante (+2). Si je dépasse la fin, 
-* je m'arrête pour ne pas lire de bêtises en mémoire.
-*/
+
+	/*
+	 * On verifie si le pointeur est NULL.
+	 */
+	if (str == NULL)
+	{
+		return;
+	}
+
 	while (str[i] != '\0')
 	{
-		_putchar(str[i]);
-
-	    i = i + 2;
-	
-        if (str[i - 1] == '\0')
+		if (i % 2 == 0)
 		{
-			break;
+			_putchar(str[i]);
 		}
+		i++;
 	}
 	_putchar('\n');
+
 }
