f1f58993017e64d6c366167cbb1b33604b1b6ac1
Marie Lopez <lopezmariegabrielle@gmail.com>
2026-03-06 12:09:31 +0100

    Add task 3

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -13,5 +13,6 @@ void swap_int(int *a, int *b);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
 
 #endif
