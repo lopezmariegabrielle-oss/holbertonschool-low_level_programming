a05051a1e8488a1de0f6e75a0de6012bc3518ede
Marie Lopez <lopezmariegabrielle@gmail.com>
2026-03-06 11:56:16 +0100

    Add task 2

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -12,5 +12,6 @@ void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
 
 #endif
