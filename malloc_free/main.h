e98c53a1f6f619723ebdfc32c7ad660227ae62ab
Marie Lopez <lopezmariegabrielle@gmail.com>
2026-03-09 12:52:11 +0100

    Add task

---
+++ malloc_free/main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * Prototypes des fonctions de la bibliothèque standard
+ * (On peut les inclure ici ou dans les fichiers .c, 
+ * mais les prototypes de tes fonctions vont ici)
+ */
+
+int _putchar(char c);
+char *create_array(unsigned int size, char c);
+
+
+
+#endif
