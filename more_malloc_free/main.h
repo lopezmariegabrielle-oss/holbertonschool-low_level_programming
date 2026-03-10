9e199a751d0fe2a38c05a66ce0922d600f79e011
Marie Lopez <lopezmariegabrielle@gmail.com>
2026-03-10 14:09:59 +0100

    Add task

---
+++ more_malloc_free/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * Prototypes des fonctions de la bibliothèque standard
+ * (On peut les inclure ici ou dans les fichiers .c, 
+ * mais les prototypes de tes fonctions vont ici)
+ */
+
+void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+
+
+#endif
