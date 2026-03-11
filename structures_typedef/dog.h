3b528eae407782660c913bb4c659fe4d96c6f74f
Marie Lopez <lopezmariegabrielle@gmail.com>
2026-03-11 13:45:04 +0100

    Add task 0

---
+++ structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Une structure regroupant les infos d'un chien
+ * @name: Premier membre (nom du chien)
+ * @age: Deuxième membre (âge du chien)
+ * @owner: Troisième membre (nom du propriétaire)
+ */
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+
+#endif /* DOG_H */
