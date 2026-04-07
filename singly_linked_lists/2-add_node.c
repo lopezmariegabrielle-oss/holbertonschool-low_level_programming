2852a81772e35db6ce220c6982d85d92bdd4e2c8
Marie Lopez <lopezmariegabrielle@gmail.com>
2026-04-07 13:17:59 +0200

    Add task 2

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new;
+
+	if (str == NULL)
+		return (NULL);
+
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->str = strdup(str);
+	if (new->str == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+	new->len = strlen(str);
+	new->next = *head;
+	*head = new;
+
+	return (new);
+}
