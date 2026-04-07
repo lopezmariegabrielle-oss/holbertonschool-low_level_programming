a6e4c30ec5254ac7aaf83119707f48a667dbcf97
Marie Lopez <lopezmariegabrielle@gmail.com>
2026-04-07 15:27:01 +0200

    Add task 4

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_list - frees a list_t list
+ * @head: pointer to the start of the list
+ */
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	while (head != NULL)
+	{
+		/*Je garde l'adreese du prochain pour ne pas le perdre */
+		temp = head->next;
+
+		/* Je libere le contenu du wagon actuel */
+		free(head->str);
+
+		/*je libere le wagon lui-meme */
+		free(head);
+		
+		/*J'avence le wagon suivant que j'ai mis de côté */
+		head = temp;
+	}
+}
